6d03df8e262c05b2f66638b61d81cdab7608252c
pavitrapathak <pavitraapathakk@gmail.com>
2026-05-26 16:06:48 +0530

    Create 3.1.1.c

---
+++ 3.1.1.c
@@ -0,0 +1,11 @@
+# Prompt the user to enter three integers
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+# Find the largest integer
+largest = max(num1, num2, num3)
+
+# Print the largest integer
+print(largest)
+
