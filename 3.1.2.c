27857fb10267fab82c2306cdecdb2334fb62f5b4
pavitrapathak <pavitraapathakk@gmail.com>
2026-05-26 16:07:20 +0530

    Create 3.1.2.c

---
+++ 3.1.2.c
@@ -0,0 +1,9 @@
+# Read temperature in Celsius from the user
+celsius = float(input())
+
+# Convert Celsius to Fahrenheit using the given formula
+fahrenheit = (celsius * 9/5) + 32
+
+# Print the temperature in Fahrenheit formatted to 2 decimal places
+print(f"{fahrenheit:.2f}")
+
